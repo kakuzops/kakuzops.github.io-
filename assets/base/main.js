4772afac258189ba0d1fbd2ddddc39b04e1eeec1
kakuzoPs <kakuzo@kakuzops.com>
2017-11-28 17:57:17 -0200

    again

--- assets/base/main.js
+++ assets/base/main.js
@@ -39,17 +39,4 @@ document.addEventListener('DOMContentLoaded', function() {
     headroom.init();
   }
   
-  var disqus_config = function () {
-    this.page.url = "https://kakuzops.github.io{{ page.url }}";  // Replace PAGE_URL with your page's canonical URL variable
-    this.page.identifier = "{{ page.url }}"; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-    };
-    
-    (function() { 
-    var d = document, s = d.createElement('script');
-    s.src = 'https://kakuzops-github-io.disqus.com/embed.js';
-    s.setAttribute('data-timestamp', +new Date());
-    (d.head || d.body).appendChild(s);
-    })();
-
-  
  
