cabb71b9c94b3556eeff76d930c5bf812c30e6fa
kakuzoPs <kakuzo@kakuzops.com>
2017-11-29 08:04:46 -0200

    comit da manhã

--- assets/base/main.js
+++ assets/base/main.js
@@ -1,16 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var app = senna.dataAttributeHandler.getApp();
-  
-    app.on('endNavigate', function(event) {
-      initNavbar();
-      fixHeroHeight();
-      trackPageViews(event);
-    });
-  
-    initNavbar();
-    fixHeroHeight();
-  });
-  
   // Keeps the primary navigation in close proximity to the user
   // by fixing it when scrolling up and disappearing when scrolling down
   var headroom;
@@ -38,5 +25,19 @@ document.addEventListener('DOMContentLoaded', function() {
   
     headroom.init();
   }
+
+  //disqus 
+  var disqus_config = function () {
+    this.page.url = "https://kakuzops.github.io{{ page.url }}";  // Replace PAGE_URL with your page's canonical URL variable
+    this.page.identifier = "{{ page.url }}"; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+    };
+    
+    (function() { 
+    var d = document, s = d.createElement('script');
+    s.src = 'https://kakuzops-github-io.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+    })();
+    
   
  
